9cb570d22707cd5b94e78267e99a14213cfd4f2d
sebgos <gos.sebastian@gmail.com>
2021-06-21 14:26:28 +0200

    feat: new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent
   ],
   imports: [
     BrowserModule,
