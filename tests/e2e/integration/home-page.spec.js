e55862e14b8b28eb08cdedde5b3dd990333fa6ff
michalkotas <miko@mikofoto.net>
2021-06-22 11:40:19 +0200

    feat: add another e2e test

--- tests/e2e/integration/home-page.spec.js
+++ tests/e2e/integration/home-page.spec.js
@@ -7,4 +7,9 @@ describe("Home Page", () => {
     // get the h3 element and verify that the app name is in it
     cy.get("app-albums h3").should("contain.text", "Albums collection");
   });
+
+  it("should display albums list", () => {
+    cy.visit("/"); // go to the home page
+    cy.get(".row.albums").children().should("have.length.gt", 0);
+  });
 });
